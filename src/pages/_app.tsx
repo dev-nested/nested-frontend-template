f937a37646cb56c8147afba95c912e4bc656c600
dev-nested <dev.nested@gmail.com>
2022-03-19 10:13:50 -0500

    add material-ui
    ref: https://github.com/mui/material-ui/tree/master/examples/nextjs-with-typescript

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,37 @@
+import * as React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from '@app/material-ui/theme';
+import createEmotionCache from '@app/material-ui/createEmotionCache';
 import '../styles/globals.css';
-
 import { Provider } from 'react-redux';
-import type { AppProps } from 'next/app';
+import store from '@app/store';
 
-import store from '../app/store';
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+interface MyAppProps extends AppProps {
+	emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
 	return (
 		<Provider store={store}>
-			<Component {...pageProps} />
+			<CacheProvider value={emotionCache}>
+				<Head>
+					<meta name="viewport" content="initial-scale=1, width=device-width" />
+				</Head>
+				<ThemeProvider theme={theme}>
+					{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+					<CssBaseline />
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</CacheProvider>
 		</Provider>
 	);
 }
